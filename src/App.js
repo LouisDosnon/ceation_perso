ae91254e9386a0690a6a52762d500553692cf76a
louis <louisdosnon@gmail.com>
2023-01-17 19:09:42 +0100

    design

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import { SelectionRaceToClasse } from './SelectionRaceToClasse';
 import { SelectionClasseToRace } from './SelectionClasseToRace';
 
@@ -6,13 +7,16 @@ import { SelectionClasseToRace } from './SelectionClasseToRace';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App background-slideshow">
       <header className="App-header">
+          <h1>Creation perso</h1>
+      </header>
+      <body className="App-body">
         <h2>Race => Classe:</h2>
         <SelectionRaceToClasse />
         <h2>Classe => Race:</h2>
         <SelectionClasseToRace />
-      </header>
+      </body>
     </div>
   );
 }
